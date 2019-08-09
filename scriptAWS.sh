4228b150080cd899a621072c3b0de0f9bc61d556
SergeyT410 <info.ok@yahoo.com>
2019-08-09 21:44:00 +0300

    fix errors but ping pong not work :(

--- scriptAWS.sh
+++ scriptAWS.sh
@@ -19,13 +19,13 @@ echo "User ubuntu"
 echo "IdentityFile /etc/ssh/NEW.pem"
 echo "Host Sacond"
 echo "HostName $Sacond"
-echo "User ubuntu"
+echo "User ec2-user"
 echo "IdentityFile /etc/ssh/NEW.pem"
 echo "Host $First"
 echo "User ubuntu" 
 echo "IdentityFile /etc/ssh/NEW.pem"
 echo "Host $Sacond"
-echo "User ubuntu"
+echo "User ec2-user"
 echo "IdentityFile /etc/ssh/NEW.pem"
 } > /etc/ssh/ssh_config
 
@@ -40,13 +40,14 @@ echo "Sacond ansible_ssh_host=$Sacond"
 #ansible group_vars
 rm -R /etc/ansible/group_vars
 mkdir /etc/ansible/group_vars
-touch /etc/ansible/group_vars/debian.yml /etc/ansible/group_vars/debian.yml
+
+touch /etc/ansible/group_vars/debian.yml
 {
 echo "---"
 echo "ansible_ssh_user: ubuntu"
 } > /etc/ansible/group_vars/debian.yml
 
-touch /etc/ansible/group_vars/debian.yml /etc/ansible/group_vars/radhat.yml
+touch /etc/ansible/group_vars/radhat.yml
 {
 echo "---"
 echo "ansible_ssh_user: ec2-user"
