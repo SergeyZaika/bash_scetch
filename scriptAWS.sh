d94be8f4a735e1b1271dece3a04f030435a6e411
SergeyT410 <info.ok@yahoo.com>
2019-08-09 19:56:32 +0300

    new script update for Debian and Ubuntu. we have 2group hosts and autumatik mkdir group_vars

--- scriptAWS.sh
+++ scriptAWS.sh
@@ -2,12 +2,14 @@
 
 IP ()
 {
-echo "First IPaddr enter:"
+echo "Debian first IPaddr enter:"
 read First
-echo "Sacond IPaddr enter:"
+echo "RadHat sacond IPaddr enter:"
 read Sacond
 }
 IP
+
+#ssh_config
 {
 echo "Host *"
 echo ""
@@ -20,18 +22,36 @@ echo "HostName $Sacond"
 echo "User ubuntu"
 echo "IdentityFile /etc/ssh/NEW.pem"
 echo "Host $First"
-echo "User ubuntu"
+echo "User ubuntu" 
 echo "IdentityFile /etc/ssh/NEW.pem"
 echo "Host $Sacond"
 echo "User ubuntu"
 echo "IdentityFile /etc/ssh/NEW.pem"
 } > /etc/ssh/ssh_config
 
+#ansible inventary hosts
 {
-echo "[servers]"
+echo "[debian]"
 echo "First ansible_ssh_host=$First"
+echo "[radhat]"
 echo "Sacond ansible_ssh_host=$Sacond"
 } > /etc/ansible/hosts
+
+#ansible group_vars
+rm -R /etc/ansible/group_vars
+mkdir /etc/ansible/group_vars
+touch /etc/ansible/group_vars/debian.yml /etc/ansible/group_vars/debian.yml
+{
+echo "---"
+echo "ansible_ssh_user: ubuntu"
+} > /etc/ansible/group_vars/debian.yml
+
+touch /etc/ansible/group_vars/debian.yml /etc/ansible/group_vars/radhat.yml
+{
+echo "---"
+echo "ansible_ssh_user: ec2-user"
+} > /etc/ansible/group_vars/radhat.yml
+
 
 
 
