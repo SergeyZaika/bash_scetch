ceb76b2ffdc823cd2b0837af40095117bea9261a
orangepi <info.ok@mail.ru>
2020-05-25 18:54:56 +0300

    update joomla_backup

--- joomla_backup.sh
+++ joomla_backup.sh
@@ -2,15 +2,16 @@
 
 echo "Starting Joomla backup!"
 date=`date '+%Y%m%d'`
-tar -cPz /var/www/html/joomla/ | ssh orangepi@000.000.000.000 "cat > /home/orangepi/joomla_backup/joomla_backup_HOME_date.tar.gz"
+tar -cPvz /var/www/html/joomla/ | ssh orangepi@0.0.0.0 "cat > /home/orangepi/joomla_backup/joomla_backup_HOME_$date.tar.gz"
 # create ~/.my.cnf (permissions need to be 600). 
 # Add this to the .my.cnf file
     #[mysqldump]
     #user=mysqluser
     #password=secret
-cd /home/ubuntu/MySQL_backup & mysqldump joomla > joomla.sql & head -n 5 joomla.sql
-tar -cPz /home/ubuntu/MySQL_backup/joomla.sql | ssh orangepi@000.000.000.000  "cat > /home/orangepi/joomla_backup/joomla_backup_MySQL_$date.sql.tar.gz"
-
+mysqldump joomla > /home/ubuntu/MySQL_backup/joomla.sql & head -n 5 joomla.sql
+tar -cz /home/ubuntu/MySQL_backup/joomla.sql | ssh orangepi@0.0.0.0 "cat > /home/orangepi/joomla_backup/joomla_backup_MySQL_$date.sql.tar.gz"
+sudo tar -cz /etc/letsencrypt/ | ssh orangepi@0.0.0.0 "cat > /home/orangepi/joomla_backup/joomla_backup_letsencrypt_$date.sql.tar.gz"
+ssh orangepi@0.0.0.0 "find /home/orangepi/joomla_backup -type f -mtime +120 -delete"
 
 
 
